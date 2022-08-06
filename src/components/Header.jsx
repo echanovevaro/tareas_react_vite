60a7013c646fc982142c668143f2d2b8d73b1bd2
echanovevaro <30557018+echanovevaro@users.noreply.github.com>
2022-08-06 20:35:08 +0200

    Pequeño cambio en el titulo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,8 @@ export const Header = () => {
 	return (
 		<div className='header'>
 			<h1>
-				block de notas <span> álvaro</span>
+				block de notas{''} Con Github {''}
+				<span> álvaro{''}</span>
 			</h1>
 		</div>
 	);
